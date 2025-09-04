91dfdcb8da32243ed244019c9b1f47a4722538d0
Hoang Van Sang <sangskyred@gmail.com>
2025-09-04 21:55:54 +0700

    auto add css

--- src/components/products.js
+++ src/components/products.js
@@ -1,18 +1,32 @@
 
 import useProduct from "../hooks/useProducts"
 import React from "react"
+import Image from "next/image"
+import Link from "next/link"
 
-export default Product = () => {
-    const {product, loading, error} = useProduct()
+const Product = () => {
+    const { product, loading, error } = useProduct()
 
-    if(loading) return<div>Loading...</div>
-    if(error) return<div>Error: {error.message}</div>
+    if (loading) return <div>Loading...</div>
+    if (error) return <div>Error: {error.message}</div>
 
-    return(
-        <section>
-            <div className="product-card">
-                
+    return (
+
+        <div className="product-container">
+            <div className="product-row">
+                {product && product.products.map((item, key) =>
+                    <div key={key} className="product-card">
+                        <Link href=""><Image src={item.images[0]} width={200} height={200} /></Link>
+                        <div className="product-info">
+                            <h4>{item.title}</h4>
+                        </div>
+                    </div>
+                )}
+
             </div>
-        </section>
+        </div>
+
     )
 }
+
+export default Product;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import Category from "../components/category";
 import SwiperComponent from "../components/SwiperComponent";
 import Layout from "../components/Layout";
-
+import Product from "../components/products";
 export const Home = () => {
     return (
         <Layout>
             <section className="slide">
                 <SwiperComponent />
+            </section>
+            <section className="products">
+                <Product/>
             </section>
 
         </Layout>
